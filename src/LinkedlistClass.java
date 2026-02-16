ae5c7a97b41c8e48b85e97d50901d7b52183a893
Pratik Kumar 31 <pkumarpratik1@gmail.com>
2026-02-16 14:35:36 +0530

    wrightnow1

--- src/LinkedlistClass.java
+++ src/LinkedlistClass.java
@@ -6,6 +6,7 @@ public class LinkedlistClass {
         LinkedList<Integer> ll = new LinkedList<>();
         ll.add(10);
         ll.add(20);
+
         ll.add(30);
         ll.add(40);
         ll.add(1234567);
